dbb134ca6e292bc15b5334be0f2021506ede2182
WinstonMDP <winstonmdp@gmail.com>
2024-03-01 23:05:21 +0300

    sm

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,54 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use anyhow::ensure;
+use anyhow::{ensure, Context, Result};
 
-/// Assertion
+/// Global asertion
 #[derive(Clone, PartialEq, Debug)]
+pub struct GA {
+    a: A,
+    vars: HashMap<String, String>, // name -> set
+    disjs: HashMap<String, HashSet<String>>,
+}
+
+impl Default for GA {
+    fn default() -> Self {
+        GA {
+            a: A {
+                set: String::new(),
+                f: F::Complex {
+                    constant: String::new(),
+                    args: vec![],
+                },
+            },
+            vars: HashMap::new(),
+            disjs: HashMap::new(),
+        }
+    }
+}
+
+/// Assretion
+#[derive(Clone, PartialEq, Debug, Ord, PartialOrd, Eq)]
 pub struct A {
     set: String,
     f: F,
 }
 
 /// Formula
-#[derive(Clone, PartialEq, Debug)]
+#[derive(Clone, PartialEq, Debug, Ord, PartialOrd, Eq)]
 pub enum F {
     Complex { constant: String, args: Vec<F> },
-    Var { name: String, set: String },
+    Var(String),
 }
 
 pub enum Step {
-    A(usize),
-    R(usize),
+    A {
+        i: usize,
+        substs: HashMap<String, F>,
+    },
+    R {
+        i: usize,
+        substs: HashMap<String, F>,
+    },
 }
 
 /// Rule
@@ -26,125 +56,124 @@ pub enum Step {
 pub struct R {
     pub hyps: Vec<A>,
     pub head: A,
+    vars: HashMap<String, String>, // name -> set
+    disjs: HashMap<String, HashSet<String>>,
 }
 
-#[must_use]
-pub fn is_proof(context: (&[A], &[R]), a: &A, steps: &[Step]) -> bool {
-    let proof_stack = process(context, steps);
-    proof_stack.len() == 1 && proof_stack[0] == *a
-}
-
-// TODO: subst step (add substs to A and R steps)
-
-#[must_use]
-pub fn process(context: (&[A], &[R]), steps: &[Step]) -> Vec<A> {
+pub fn process(
+    context: (&[GA], &[R]),
+    vars_map: &HashMap<String, String>,
+    steps: &[Step],
+) -> Result<Vec<A>> {
     let mut proof_stack = vec![];
+    let mut disjs: HashMap<String, HashSet<String>> = HashMap::new();
     for step in steps {
         match step {
-            Step::A(i) => proof_stack.push(context.0[*i].clone()),
-            Step::R(i) => {
+            Step::A { i, substs } => {
+                let mut ga = context.0[*i].clone();
+                scroll(
+                    &mut disj_check(vars_map, &ga.vars, &ga.disjs, substs, &mut disjs)?,
+                    &mut proof_stack,
+                )?;
+                subst(&mut ga.a.f, substs)?;
+                proof_stack.push(ga.a);
+            }
+            Step::R { i, substs } => {
                 let mut r = context.1[*i].clone();
-                let mut substs = HashMap::new();
-                while let Some(mut hyp) = r.hyps.pop() {
-                    println!("subst hyp {:?} with {substs:?}", hyp.f);
-                    subst(&substs, &mut hyp.f);
-                    println!("the hyp {:?} after subst", hyp.f);
-                    let a = proof_stack.pop().unwrap();
-                    println!("take {a:?} from proof stack and unify");
-                    let mut u = unify(hyp.f, a.f, &mut substs).unwrap();
-                    r.hyps.append(&mut u);
-                    println!("post hyps: {:?}", r.hyps);
-                    println!("post substs: {substs:?}");
+                scroll(
+                    &mut disj_check(vars_map, &r.vars, &r.disjs, substs, &mut disjs)?,
+                    &mut proof_stack,
+                )?;
+                println!("substs: {substs:?}");
+                for hyp in &mut r.hyps {
+                    subst(&mut hyp.f, substs)?;
                 }
-                subst(&substs, &mut r.head.f);
+                scroll(&mut r.hyps, &mut proof_stack)?;
+                subst(&mut r.head.f, substs)?;
                 proof_stack.push(r.head);
             }
         }
     }
-    proof_stack
+    Ok(proof_stack)
 }
 
-fn subst(substs: &HashMap<String, F>, f: &mut F) {
-    match f {
-        F::Complex { args, .. } => {
-            for arg in args {
-                subst(substs, arg);
-            }
+// Order of result
+fn disj_check(
+    g_vars_map: &HashMap<String, String>,
+    vars_map: &HashMap<String, String>,
+    from_disjs: &HashMap<String, HashSet<String>>,
+    substs: &HashMap<String, F>,
+    to_disjs: &mut HashMap<String, HashSet<String>>,
+) -> Result<Vec<A>> {
+    let mut hyps = vec![];
+    for (name, f) in substs {
+        let set = vars_map.get(name).unwrap().to_string();
+        let proven = if let F::Var(name) = f {
+            *g_vars_map.get(name).unwrap() == set
+        } else {
+            false
+        };
+        if !proven {
+            hyps.push(A { set, f: f.clone() });
         }
-        F::Var { name, .. } => {
-            if let Some(subst_f) = substs.get(name) {
-                *f = subst_f.clone();
+        if let Some(disjs) = from_disjs.get(name) {
+            for disj in disjs {
+                let disj_f = substs.get(disj).unwrap();
+                for f_var in vars(f) {
+                    for disj_f_var in vars(disj_f) {
+                        match f_var.cmp(&disj_f_var) {
+                            std::cmp::Ordering::Less => {
+                                to_disjs.get_mut(&f_var).unwrap().insert(disj_f_var);
+                            }
+                            std::cmp::Ordering::Equal => anyhow::bail!(""),
+                            std::cmp::Ordering::Greater => {
+                                to_disjs.get_mut(&disj_f_var).unwrap().insert(f_var.clone());
+                            }
+                        };
+                    }
+                }
             }
         }
     }
+    hyps.sort_unstable();
+    Ok(hyps)
 }
 
-fn unify(hyp: F, a: F, substs: &mut HashMap<String, F>) -> anyhow::Result<Vec<A>> {
-    Ok(match hyp {
-        F::Complex {
-            constant: x_constant,
-            args: x_args,
-        } => match a {
-            F::Complex {
-                constant: y_constant,
-                args: y_args,
-            } => {
-                ensure!(x_constant == y_constant);
-                ensure!(x_args.len() == y_args.len());
-                let mut hyps = vec![];
-                x_args.into_iter().zip(y_args).for_each(|(x, y)| {
-                    let mut u = unify(x, y, substs).unwrap();
-                    hyps.append(&mut u);
-                });
-                hyps
+fn vars(f: &F) -> HashSet<String> {
+    match f {
+        F::Complex { args, .. } => {
+            let mut acc = HashSet::new();
+            for arg in args {
+                acc.extend(vars(arg));
             }
-            F::Var { set: y_set, .. } => vec![A {
-                set: y_set,
-                f: F::Complex {
-                    constant: x_constant,
-                    args: x_args,
-                },
-            }],
-        },
-        F::Var {
-            name: x_name,
-            set: x_set,
-        } => match a {
-            F::Complex {
-                constant: y_constant,
-                args: y_args,
-            } => {
-                substs.insert(
-                    x_name,
-                    F::Complex {
-                        constant: y_constant.clone(),
-                        args: y_args.clone(),
-                    },
-                );
-                vec![A {
-                    set: x_set,
-                    f: F::Complex {
-                        constant: y_constant,
-                        args: y_args,
-                    },
-                }]
+            acc
+        }
+        F::Var(name) => HashSet::from([name.clone()]),
+    }
+}
+
+fn scroll(hyps: &mut Vec<A>, proof_stack: &mut Vec<A>) -> Result<()> {
+    println!("hyps: {hyps:?}");
+    println!("proof_stack: {proof_stack:?}");
+    while let Some(hyp) = hyps.pop() {
+        ensure!(hyp == proof_stack.pop().context("")?);
+    }
+    Ok(())
+}
+
+fn subst(f: &mut F, substs: &HashMap<String, F>) -> Result<()> {
+    match f {
+        F::Complex { args, .. } => {
+            for arg in args {
+                subst(arg, substs)?;
             }
-            F::Var {
-                name: y_name,
-                set: y_set,
-            } => {
-                ensure!(x_set == y_set);
-                substs.insert(
-                    x_name,
-                    F::Var {
-                        name: y_name,
-                        set: y_set,
-                    },
-                );
-                vec![]
-            }
-        },
-    })
+        }
+        F::Var(name) => {
+            let subst_f = substs.get(name).unwrap();
+            *f = subst_f.clone();
+        }
+    }
+    Ok(())
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -1,154 +1,29 @@
 use super::*;
 
-// a: (x : wff) \in p
-// F \in p > T \in p
-
-#[test]
-fn unify_t_1() {
-    let mut substs = HashMap::new();
-    assert_eq!(
-        unify(
-            F::Complex {
-                constant: "T".to_string(),
-                args: vec![],
-            },
-            F::Complex {
-                constant: "T".to_string(),
-                args: vec![],
-            },
-            &mut substs,
-        )
-        .unwrap(),
-        vec![]
-    );
-    assert_eq!(substs, HashMap::new());
-}
-
-#[test]
-fn unify_t_2() {
-    let mut substs = HashMap::new();
-    assert_eq!(
-        unify(
-            F::Var {
-                name: "x".to_string(),
-                set: "wff".to_string(),
-            },
-            F::Complex {
-                constant: "T".to_string(),
-                args: vec![],
-            },
-            &mut substs,
-        )
-        .unwrap(),
-        vec![A {
-            set: "wff".to_string(),
-            f: F::Complex {
-                constant: "T".to_string(),
-                args: vec![]
-            }
-        }]
-    );
-    assert_eq!(
-        substs,
-        HashMap::from([(
-            "x".to_string(),
-            F::Complex {
-                constant: "T".to_string(),
-                args: vec![]
-            }
-        )])
-    );
-}
-
-#[test]
-fn unify_t_3() {
-    let mut substs = HashMap::new();
-    assert_eq!(
-        unify(
-            F::Var {
-                name: "x".to_string(),
-                set: "wff".to_string(),
-            },
-            F::Var {
-                name: "y".to_string(),
-                set: "wff".to_string()
-            },
-            &mut substs,
-        )
-        .unwrap(),
-        vec![]
-    );
-    assert_eq!(
-        substs,
-        HashMap::from([(
-            "x".to_string(),
-            F::Var {
-                name: "y".to_string(),
-                set: "wff".to_string()
-            }
-        )])
-    );
-}
-
-#[test]
-fn subst_t_1() {
-    let mut f = F::Var {
-        name: "x".to_string(),
-        set: "wff".to_string(),
-    };
-    subst(
-        &HashMap::from([(
-            "x".to_string(),
-            F::Complex {
-                constant: "T".to_string(),
-                args: vec![],
-            },
-        )]),
-        &mut f,
-    );
-    assert_eq!(
-        f,
-        F::Complex {
-            constant: "T".to_string(),
-            args: vec![],
-        }
-    );
-}
-
 fn mp() -> R {
     R {
         hyps: vec![
             A {
                 set: "p".to_string(),
-                f: F::Var {
-                    name: "x".to_string(),
-                    set: "wff".to_string(),
-                },
+                f: F::Var("x".to_string()),
             },
             A {
                 set: "p".to_string(),
                 f: F::Complex {
                     constant: "imp".to_string(),
-                    args: vec![
-                        F::Var {
-                            name: "x".to_string(),
-                            set: "wff".to_string(),
-                        },
-                        F::Var {
-                            name: "y".to_string(),
-                            set: "wff".to_string(),
-                        },
-                    ],
+                    args: vec![F::Var("x".to_string()), F::Var("y".to_string())],
                 },
             },
         ],
         head: A {
             set: "p".to_string(),
-            f: F::Var {
-                name: "y".to_string(),
-                set: "p".to_string(),
-            },
+            f: F::Var("y".to_string()),
         },
+        vars: HashMap::from([
+            ("x".to_string(), "wff".to_string()),
+            ("y".to_string(), "wff".to_string()),
+        ]),
+        disjs: HashMap::new(),
     }
 }
 
@@ -157,17 +32,25 @@ fn process_t_1() {
     assert_eq!(
         process(
             (
-                &[A {
-                    set: "p".to_string(),
-                    f: F::Complex {
-                        constant: "T".to_string(),
-                        args: vec![]
-                    }
+                &[GA {
+                    a: A {
+                        set: "p".to_string(),
+                        f: F::Complex {
+                            constant: "T".to_string(),
+                            args: vec![]
+                        }
+                    },
+                    ..Default::default()
                 }],
                 &[]
             ),
-            &[Step::A(0)]
-        ),
+            &HashMap::new(),
+            &[Step::A {
+                i: 0,
+                substs: HashMap::new()
+            }]
+        )
+        .unwrap(),
         vec![A {
             set: "p".to_string(),
             f: F::Complex {
@@ -184,48 +67,98 @@ fn process_t_2() {
         process(
             (
                 &[
-                    A {
-                        set: "p".to_string(),
-                        f: F::Complex {
-                            constant: "F".to_string(),
-                            args: vec![]
-                        }
+                    GA {
+                        a: A {
+                            set: "p".to_string(),
+                            f: F::Complex {
+                                constant: "F".to_string(),
+                                args: vec![]
+                            }
+                        },
+                        ..Default::default()
                     },
-                    A {
-                        set: "p".to_string(),
-                        f: F::Complex {
-                            constant: "imp".to_string(),
-                            args: vec![
-                                F::Complex {
-                                    constant: "F".to_string(),
-                                    args: vec![]
-                                },
-                                F::Complex {
-                                    constant: "T".to_string(),
-                                    args: vec![]
-                                }
-                            ]
-                        }
+                    GA {
+                        a: A {
+                            set: "p".to_string(),
+                            f: F::Complex {
+                                constant: "imp".to_string(),
+                                args: vec![
+                                    F::Complex {
+                                        constant: "F".to_string(),
+                                        args: vec![]
+                                    },
+                                    F::Complex {
+                                        constant: "T".to_string(),
+                                        args: vec![]
+                                    }
+                                ]
+                            }
+                        },
+                        ..Default::default()
                     },
-                    A {
-                        set: "wff".to_string(),
-                        f: F::Complex {
-                            constant: "F".to_string(),
-                            args: vec![]
-                        }
+                    GA {
+                        a: A {
+                            set: "wff".to_string(),
+                            f: F::Complex {
+                                constant: "F".to_string(),
+                                args: vec![]
+                            }
+                        },
+                        ..Default::default()
                     },
-                    A {
-                        set: "wff".to_string(),
-                        f: F::Complex {
-                            constant: "T".to_string(),
-                            args: vec![]
-                        }
+                    GA {
+                        a: A {
+                            set: "wff".to_string(),
+                            f: F::Complex {
+                                constant: "T".to_string(),
+                                args: vec![]
+                            }
+                        },
+                        ..Default::default()
                     }
                 ],
                 &[mp()]
             ),
-            &[Step::A(0), Step::A(2), Step::A(3), Step::A(1), Step::R(0)]
-        ),
+            &HashMap::new(),
+            &[
+                Step::A {
+                    i: 0,
+                    substs: HashMap::new()
+                },
+                Step::A {
+                    i: 1,
+                    substs: HashMap::new()
+                },
+                Step::A {
+                    i: 2,
+                    substs: HashMap::new()
+                },
+                Step::A {
+                    i: 3,
+                    substs: HashMap::new()
+                },
+                Step::R {
+                    i: 0,
+                    substs: HashMap::from([
+                        (
+                            "x".to_string(),
+                            F::Complex {
+                                constant: "F".to_string(),
+                                args: vec![]
+                            }
+                        ),
+                        (
+                            "y".to_string(),
+                            F::Complex {
+                                constant: "T".to_string(),
+                                args: vec![]
+                            }
+                        )
+                    ])
+                }
+            ]
+        )
+        .unwrap(),
         vec![A {
             set: "p".to_string(),
             f: F::Complex {
@@ -242,84 +175,136 @@ fn process_t_3() {
         process(
             (
                 &[
-                    A {
-                        set: "p".to_string(),
-                        f: F::Var {
-                            name: "z".to_string(),
-                            set: "wff".to_string(),
+                    GA {
+                        a: A {
+                            set: "p".to_string(),
+                            f: F::Var("q".to_string()),
                         },
+                        vars: HashMap::from([("q".to_string(), "wff".to_string())]),
+                        ..Default::default()
                     },
-                    A {
-                        set: "p".to_string(),
-                        f: F::Complex {
-                            constant: "imp".to_string(),
-                            args: vec![
-                                F::Var {
-                                    name: "z".to_string(),
-                                    set: "wff".to_string(),
-                                },
-                                F::Var {
-                                    name: "w".to_string(),
-                                    set: "wff".to_string(),
-                                },
-                            ],
+                    GA {
+                        a: A {
+                            set: "p".to_string(),
+                            f: F::Complex {
+                                constant: "imp".to_string(),
+                                args: vec![F::Var("x".to_string()), F::Var("y".to_string()),],
+                            },
                         },
+                        vars: HashMap::from([
+                            ("x".to_string(), "wff".to_string()),
+                            ("y".to_string(), "wff".to_string())
+                        ]),
+                        ..Default::default()
                     },
                 ],
                 &[mp()]
             ),
-            &[Step::A(0), Step::A(1), Step::R(0)]
-        ),
+            &HashMap::from([
+                ("z".to_string(), "wff".to_string()),
+                ("w".to_string(), "wff".to_string())
+            ]),
+            &[
+                Step::A {
+                    i: 0,
+                    substs: HashMap::from([("q".to_string(), F::Var("z".to_string()))])
+                },
+                Step::A {
+                    i: 1,
+                    substs: HashMap::from([
+                        ("x".to_string(), F::Var("z".to_string())),
+                        ("y".to_string(), F::Var("w".to_string()))
+                    ])
+                },
+                Step::R {
+                    i: 0,
+                    substs: HashMap::from([
+                        ("x".to_string(), F::Var("z".to_string())),
+                        ("y".to_string(), F::Var("w".to_string()))
+                    ])
+                }
+            ]
+        )
+        .unwrap(),
         vec![A {
             set: "p".to_string(),
-            f: F::Var {
-                name: "w".to_string(),
-                set: "wff".to_string()
-            }
+            f: F::Var("w".to_string())
         }],
     );
 }
 
 #[test]
 fn process_t_4() {
-    assert_eq!(
-        process(
-            (
-                &[
-                    A {
-                        set: "p".to_string(),
-                        f: F::Var {
-                            name: "x".to_string(),
-                            set: "wff".to_string(),
-                        },
+    process(
+        (
+            &[GA {
+                a: A {
+                    set: "p".to_string(),
+                    f: F::Complex {
+                        constant: "imp".to_string(),
+                        args: vec![F::Var("x".to_string()), F::Var("y".to_string())],
                     },
-                    A {
-                        set: "p".to_string(),
-                        f: F::Complex {
-                            constant: "imp".to_string(),
-                            args: vec![
-                                F::Var {
-                                    name: "x".to_string(),
-                                    set: "wff".to_string(),
-                                },
-                                F::Var {
-                                    name: "y".to_string(),
-                                    set: "wff".to_string(),
-                                },
-                            ],
-                        },
-                    },
-                ],
-                &[mp()]
-            ),
-            &[Step::A(0), Step::A(1), Step::R(0)]
+                },
+                vars: HashMap::from([
+                    ("x".to_string(), "wff".to_string()),
+                    ("y".to_string(), "wff".to_string()),
+                ]),
+                disjs: HashMap::from([("x".to_string(), HashSet::from(["y".to_string()]))]),
+            }],
+            &[],
         ),
-        vec![A {
-            set: "p".to_string(),
-            f: F::Var {
-                name: "y".to_string(),
-                set: "wff".to_string()
-            }
+        &HashMap::from([("x".to_string(), "wff".to_string())]),
+        &[Step::A {
+            i: 0,
+            substs: HashMap::from([
+                ("x".to_string(), F::Var("x".to_string())),
+                ("y".to_string(), F::Var("x".to_string())),
+            ]),
         }],
-    );
+    )
+    .unwrap_err();
+}
+
+#[test]
+fn process_t_5() {
+    process(
+        (
+            &[GA {
+                a: A {
+                    set: "p".to_string(),
+                    f: F::Complex {
+                        constant: "imp".to_string(),
+                        args: vec![F::Var("x".to_string()), F::Var("y".to_string())],
+                    },
+                },
+                vars: HashMap::from([
+                    ("x".to_string(), "wff".to_string()),
+                    ("y".to_string(), "wff".to_string()),
+                ]),
+                disjs: HashMap::from([("x".to_string(), HashSet::from(["y".to_string()]))]),
+            }],
+            &[],
+        ),
+        &HashMap::from([("x".to_string(), "wff".to_string())]),
+        &[Step::A {
+            i: 0,
+            substs: HashMap::from([
+                (
+                    "x".to_string(),
+                    F::Complex {
+                        constant: "T".to_string(),
+                        args: vec![F::Var("x".to_string())],
+                    },
+                ),
+                (
+                    "y".to_string(),
+                    F::Complex {
+                        constant: "T".to_string(),
+                        args: vec![F::Var("x".to_string())],
+                    },
+                ),
+            ]),
+        }],
+    )
+    .unwrap_err();
 }
